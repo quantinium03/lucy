b23560dca3d7cf96f558522a49f06b7a12196bf6
quantinium03 <vaibhavvx331@gmail.com>
2025-01-08 04:01:19 +0530

    added model, handler for keyboard

--- internal/database/model/keyboard.go
+++ internal/database/model/keyboard.go
@@ -1,9 +1,17 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Keyboard struct {
 	ID uuid.UUID `gorm:"type:uuid"`
-	Username string `gorm:"unique;not null;primaryKey" json:"username"`
-	Keypress uint64 `json:"keypress"`
+	Username string `gorm:"unique;not null" json:"username"`
+	Keypress uint64 `gorm:"default:0" json:"keypress"`
+}
+
+func (keyboard * Keyboard) BeforeCreate(tx *gorm.DB) (err error) {
+	keyboard.ID = uuid.New()
+	return
 }
